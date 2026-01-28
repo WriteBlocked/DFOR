81e0cb4de2d1192f5d99ae699e862203441c8707
Hiller <hoover.hiller@gmail.com>
2026-01-27 19:23:29 -0500

    first commit

---
+++ DFOR 772/Hoover_Hiller_hw1_stub.py
@@ -0,0 +1,29 @@
+""" DFOR 772
+    Hiller Hoover """
+
+version = '0.1'
+
+""" Change Log:
+    01/21/2026: added stub functions, updated version."""
+
+def parse_arguments():
+    pass
+
+def open_image():
+     pass
+
+def retrieve_partition_info():
+    pass
+
+def output_partition_info():
+    pass
+
+def main():
+    open_image()
+    parse_arguments()
+    retrieve_partition_info()
+    output_partition_info()
+    print("Finished running stubs.")
+
+if __name__ == '__main__':
+    main()
---
+++ DFOR 772/Hoover_Hiller_hw1_template.py
@@ -0,0 +1,19 @@
+""" DFOR 772
+    Hiller Hoover """
+
+version = '0.0'
+
+""" Change Log:
+    MM/DD/YY: actions... """
+
+""" import xyz """
+
+def parse_arguments():
+    pass
+
+def main():
+    parse_arguments()
+    pass
+
+if __name__ == '__main__':
+    main()
